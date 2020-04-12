3852055cfc1af33650218d09eec292a420e30144
thedevtron <anthony@thedevtron.com>
2020-04-12 14:35:32 -0400

    Remove Experiment

--- new/assets/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'ricoadmin');
-define('DB_PASSWORD', '&y9Klc05');
-define('DB_NAME', 'rico');
-?>
