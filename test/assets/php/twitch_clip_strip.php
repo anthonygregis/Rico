e7766d5092007a76d90c8ad5b046bcd0278f2229
thedevtron <anthony@thedevtron.com>
2020-04-12 22:32:00 -0400

    Strip Test

--- test/assets/php/twitch_clip_strip.php
+++ test/assets/php/twitch_clip_strip.php
@@ -1,10 +1,10 @@
 <?php
 
-$URL = "https://clips.twitch.tv/BlatantAmusedArmadilloThunBeast";
+$URL = "https://www.udemy.com/new-lecture/view/?data=SlFCJUxYR34JQx55Xk5XPFAUTh4NDRsqWBQJe0wBAW5M";
 $Fetched_Contents = file_get_contents($URL);
-
 if (preg_match('/<source(.*?)src="(.*?).mp4(.*?)"/i', $Fetched_Contents, $MP4_Link)){
-    $video = "{$MP4_Link[2]}.mp4{$MP4_Link[3]}";
+    $Complete_MP4_Link = "{$MP4_Link[2]}.mp4{$MP4_Link[3]}";
+    echo $Complete_MP4_Link;
 }else{
     echo "Didn't found any mp4 link.";
 }
--- test/index.php
+++ test/index.php
@@ -83,7 +83,18 @@ require_once 'assets/php/twitch_clip_strip.php';
         <p>Payment Type: <?php echo $row['paymentType'] ?></p> 
         <p>Payment Amount: <?php echo $row['paymentAmount'] ?></p> 
         <p>Posted At: <?php echo $row['crimeTime'] ?></p> 
-        <video width="80%" autoplay loop><source src="<?php echo $video;?>" type="video/mp4"></video>
+        <video width="80%" autoplay loop><source src="<?php
+
+$URL = "https://www.udemy.com/new-lecture/view/?data=SlFCJUxYR34JQx55Xk5XPFAUTh4NDRsqWBQJe0wBAW5M";
+$Fetched_Contents = file_get_contents($URL);
+if (preg_match('/<source(.*?)src="(.*?).mp4(.*?)"/i', $Fetched_Contents, $MP4_Link)){
+    $Complete_MP4_Link = "{$MP4_Link[2]}.mp4{$MP4_Link[3]}";
+    echo $Complete_MP4_Link;
+}else{
+    echo "Didn't found any mp4 link.";
+}
+
+?>" type="video/mp4"></video>
       <br>
   </section>
   <?php } ?>
