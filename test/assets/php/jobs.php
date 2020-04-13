73e8488ecc9ee025aa32e360920b89ede1357b20
thedevtron <anthony@thedevtron.com>
2020-04-12 21:47:47 -0400

    Add Assets

---
+++ test/assets/php/jobs.php
@@ -0,0 +1,35 @@
+<?php 
+require_once dirname( __FILE__ ) . '/' . "../config/config.php";
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+if(!isset($sort) || $sort == "time") {
+    $query = "SELECT * FROM jobs ORDER BY sponsoredJob DESC, crimeTime DESC";
+} else if($sort == "value") {
+    $query = "SELECT * FROM jobs ORDER BY sponsoredJob DESC, paymentAmount DESC";
+}
+
+$result = mysqli_query($link, $query) or die(mysqli_error($link));
+$tableArray = array();
+$counter = 0;
+while ($row = mysqli_fetch_array($result))
+{
+     $tableArray[$counter]['counter'] = $counter;
+     $tableArray[$counter]['crime'] = $row['crime'];
+     $tableArray[$counter]['crimeUuid'] = $row['crimeUuid'];
+     $tableArray[$counter]['sponsoredJob'] = $row['sponsoredJob'];
+     $tableArray[$counter]['paymentType'] = $row['paymentType'];
+	 $tableArray[$counter]['paymentAmount'] = $row['paymentAmount'];
+     $tableArray[$counter]['crimeTime'] = date('M d h:i A',strtotime($row['crimeTime']));
+     $counter++;
+}
+
+//Close Connection
+mysqli_close($link);
+?>
