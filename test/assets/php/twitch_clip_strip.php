3490bd0788ed735c022820ac948cc170780c9fbb
thedevtron <anthony@thedevtron.com>
2020-04-12 22:49:25 -0400

    Strip Test v4

--- test/assets/php/twitch_clip_strip.php
+++ test/assets/php/twitch_clip_strip.php
@@ -1,12 +1,8 @@
 <?php
 
-$URL = "https://www.udemy.com/new-lecture/view/?data=SlFCJUxYR34JQx55Xk5XPFAUTh4NDRsqWBQJe0wBAW5M";
-$Fetched_Contents = file_get_contents($URL);
-if (preg_match('/<source(.*?)src="(.*?).mp4(.*?)"/i', $Fetched_Contents, $MP4_Link)){
-    $Complete_MP4_Link = "{$MP4_Link[2]}.mp4{$MP4_Link[3]}";
-    echo $Complete_MP4_Link;
-}else{
-    echo "Didn't found any mp4 link.";
-}
+$source = file_get_contents("https://clips.twitch.tv/BlatantAmusedArmadilloThunBeast");
+
+preg_match('<video playsinline="" webkit-playsinline="" src=\"(.*?)\">', $source, $match);
+if($match) $video = $match[1];
 
 ?>
