666ed157ffe4ec1a9ed9cb2eb134b32dcb33c9e3
thedevtron <anthony@thedevtron.com>
2020-04-12 23:23:54 -0400

    Remove Test Directory

--- test/assets/js/strip.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var files = fs.readdirSync("https://clips.twitch.tv/TalentedDirtyPineappleKappa");
-var path = require('path');
-
-for(var i in files) {
-   if(path.extname(files[i]) === ".mp4") {
-    console.log(i);
-   }
-}
