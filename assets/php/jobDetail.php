5c2ae488983b640afd1357b3a8477d277410cf9c
thedevtron <anthony@thedevtron.com>
2020-04-14 12:00:05 -0400

    Counter Fix

--- assets/php/jobDetail.php
+++ assets/php/jobDetail.php
@@ -34,7 +34,7 @@ if(isset($job)) {
 }
 
 $result = mysqli_query($link, $query2) or die(mysqli_error($link));
-$counter = 0;
+$counter = 1;
 while ($row = mysqli_fetch_array($result))
 {
      $workerClaims = $counter;
