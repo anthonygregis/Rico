2b4f9a5b9ecaa325609b31764ef484fa18095314
thedevtron <anthony@thedevtron.com>
2020-04-14 11:35:19 -0400

    Crime Table Change

--- assets/php/jobDetail.php
+++ assets/php/jobDetail.php
@@ -10,7 +10,7 @@ if($link === false){
 }
 
 if(isset($job)) {
-    $query = "SELECT * FROM jobs WHERE crimeUuid = '$job'";
+    $query = "SELECT * FROM crimes WHERE crimeUuid = '$job'";
 }
 
 $result = mysqli_query($link, $query) or die(mysqli_error($link));
