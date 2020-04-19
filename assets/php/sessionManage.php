5549a1cc2e26e9bb115451174a13d9ad3f468fd1
thedevtron <anthony@thedevtron.com>
2020-04-19 18:17:16 -0400

    Remove testing session

--- assets/php/sessionManage.php
+++ assets/php/sessionManage.php
@@ -4,9 +4,5 @@ if (session_id() === "") {
     session_id('ricoTracking');
     session_start();
 }
-  
-$_SESSION['userUuid'] = "1";
-$_SESSION['userIdentifier'] = "Bananas";
-$_SESSION['userContractor'] = 1;
 
 ?>
